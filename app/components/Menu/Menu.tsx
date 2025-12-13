fedc303b34598c6dbd52793be5027e1225a1816c
Danil Belokon <ilya.belokon.07@gmail.com>
2025-12-13 14:05:09 +0300

    Changing the sidebar and menu to client-side components. Making the header animated.

--- app/components/Menu/Menu.tsx
+++ app/components/Menu/Menu.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import getMenu from "@/lib/api/menu";
-import {JSX} from "react";
+import {JSX, useEffect, useState} from "react";
 import styles from "./Menu.module.css";
 import MenuClient from "./MenuClient";
+import {MenuItem} from "@/interfaces/menu.interface";
 
-export default async function Menu(): Promise<JSX.Element> {
+export default function Menu() {
+  const [menu, setMenu] = useState<MenuItem[]>([]);
+  const [loading, setLoading] = useState(true); // ← добавить
   const firstCategory = 0;
-  const menu = await getMenu(firstCategory);
+  useEffect(() => {
+    const fetchGetMenu = async () => {
+      try {
+        const res = await getMenu(firstCategory);
+        setMenu(res);
+      } catch (error) {
+        console.error("Ошибка загрузки меню:", error);
+      } finally {
+        setLoading(false); // ← добавить
+      }
+    };
+    fetchGetMenu();
+  }, []);
+
+  if (loading) {
+    return <div className={styles.menu}>Загрузка...</div>;
+  }
 
   return (
     <div className={styles.menu}>
-      <MenuClient firstCategory={firstCategory} menu={menu}></MenuClient>
+      {menu && (
+        <MenuClient firstCategory={firstCategory} menu={menu}></MenuClient>
+      )}
     </div>
   );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,56 @@
-import React, {JSX} from "react";
-// import styles from "./Header.module.css";
-import {HeaderProps} from "./Header.props";
+"use client";
 
-export const Header = ({...props}: HeaderProps): JSX.Element => {
-  return <div {...props}>Header</div>;
+import React, {JSX, useEffect, useState} from "react";
+import styles from "./Header.module.css";
+import {HeaderProps} from "./Header.props";
+import clsx from "clsx";
+import Logo from "../../public/logo.svg";
+import {ButtonIcon} from "../ButtonIcon/ButtonIcon";
+import {motion} from "framer-motion";
+import {Sidebar} from "../Sidebar/Sidebar";
+import {usePathname} from "next/navigation";
+
+export const Header = ({className, ...props}: HeaderProps): JSX.Element => {
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+  const router = usePathname();
+  const variants = {
+    opened: {
+      opacity: 1,
+      x: 0,
+      // transition: {
+      //   stiffness: 20,
+      // },
+    },
+    closed: {
+      opacity: 0,
+      x: "100%",
+    },
+  };
+  useEffect(() => {
+    setIsOpened(false);
+  }, [router]);
+  return (
+    <header className={clsx(className, styles.header)} {...props}>
+      <Logo></Logo>
+      <ButtonIcon
+        appearance="white"
+        icon="menu"
+        onClick={() => setIsOpened(true)}
+      ></ButtonIcon>
+      <motion.div
+        variants={variants}
+        initial="closed"
+        animate={isOpened ? "opened" : "closed"}
+        className={styles.mobileMenu}
+      >
+        <Sidebar></Sidebar>
+        <ButtonIcon
+          className={styles.menuClose}
+          appearance="white"
+          icon="menu"
+          onClick={() => setIsOpened(false)}
+        ></ButtonIcon>
+      </motion.div>
+    </header>
+  );
 };
